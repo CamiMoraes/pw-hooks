3012f51ee7ceb0685311a2b847f5e2a4d6c8b749
CamiMoraes <camilasoumoraes@gmail.com>
2021-06-06 14:18:10 +0000

    hooks

---
+++ hooks/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import reactDOM from 'react-dom'
+import React from 'react'
+
+import App from './views/App'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
