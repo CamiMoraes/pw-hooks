fa42f16b99abeafac6c5bc91b16412f878d2df3d
CamiMoraes <camilasoumoraes@gmail.com>
2021-09-12 01:16:37 +0000

    Aula 14

--- hooks/src/views/contents/UseState.jsx
+++ hooks/src/views/contents/UseState.jsx
@@ -1,41 +1,58 @@
 import './UseState.css'
-import React, { useState } from "react"
+import React, { useState, useContext } from 'react'
+import { AppContexto } from '../../data/Store'
 
 const UseState = props => {
+    const { contextoNome, setContextoNome } = useContext(AppContexto)
 
-    const [nome,setNome] = useState("")
-    const [sobrenome,setSobrenome] = useState("")
-    const [mensagem,setMensagem] = useState("")
+    const [nome, setNome] = useState('')
+    const [sobrenome, setSobrenome] = useState('')
+    const [mensagem, setMensagem] = useState('')
 
-return (
-    <div>
-        <div className="titulo">
-            <h1>UseState</h1>
-            <h2> Manipula Estados em componentes funcionais</h2>
-        </div>
-        <div className="conteudo">
-            <input
-                className="campo"
-                type="text" 
-                value={nome}
-                placeholder="Digite seu Nome"
-                onChange={ e => setNome(e.target.value)}
+    return (
+        <div>
+            <div className="titulo">
+                <h1>useState</h1>
+                <h3>Manipula Estados em componentes funcionais</h3>
+            </div>
+
+            <div className="conteudo">
+                <input 
+                    className="campo"
+                    type="text" value={nome}
+                    placeholder="Digite seu nome"
+                    onChange={e => setNome(e.target.value)}
                 />
-               <input
-                className="campo"
-                type="text" 
-                value={sobrenome}
-                placeholder="Digite seu sobrenome"
-                onChange={ e => setSobrenome(e.target.value)}
+                <input
+                    className="campo"
+                    type="text"
+                    value={sobrenome}
+                    placeholder="Digite seu sobrenome"
+                    onChange={e => setSobrenome(e.target.value)}
                 />
                 <button
-                className="botao"
-                onClick={ () => setMensagem(`Olá ${nome} ${sobrenome}, seja bem-vindo`)}
+                    className="botao"
+                    onClick={() => {
+                        if (nome === '' || sobrenome === '') {
+                            alert('Preencha todos os campos!')
+                        } else {
+                           setMensagem(`Olá ${nome} ${sobrenome}, seja bem-vindo!`)
+                        }
+                        }
+                    }
                 >OK</button>
 
                 <p className="campo">{mensagem}</p>
+
+                <button
+                    className="botao"
+                    onClick={() => setContextoNome(nome)}
+                >
+                    Altera contexto (Store)
+                </button>
+            </div>
         </div>
-    </div>
-)
+    )
 }
+
 export default UseState
